9a0153d2ca3352d5177ad495a77f499acff30a0d
Abdul Samad <abdulsamamd9004607@gmail.com>
2021-04-30 12:22:11 +0500

    js assignment 5

---
+++ app.js
@@ -0,0 +1,106 @@
+//******************************* Chapter : 17-20 ********************************
+
+//************************** Task : 01 ***************************
+
+// var arr = [
+//     [],
+//     [],
+//     []
+// ]
+
+
+//************************** Task : 02 ***************************
+
+// var arr = [
+//     [0,1,2,3],
+//     [1,0,1,2],
+//     [2,1,0,1]
+// ]
+
+// for(var a = 0; a < arr.length; a++){
+//     for(var b = 0; b < arr[a].length; b++){
+        
+//         document.write(arr[a][b] + ' ')
+//     }
+//     document.write('<br>')
+// }
+
+
+//************************** Task : 03 ***************************
+
+// for(var a = 1; a<= 10; a++){
+//     document.write(a + '<br>')
+// }
+
+
+//************************** Task : 04 ***************************
+
+// var userInput1 = +prompt('Enter number to show its multiplication table');
+// var userInput2 = +prompt('Enter number how many times long');
+// for(var a = 1; a <= userInput2; a++){
+//     document.write(userInput1 + ' x ' + a + ' = ' + userInput1*a + '<br>')
+// }
+
+
+//************************** Task : 05 ***************************
+
+// var fruits = ['apple', 'banana', 'mango', 'orange','strawberry']
+    
+// for(var a = 0; a < fruits.length; a++){
+//     document.write('Element at index ' + a + ' is ' + fruits[a] + '<br>')
+// }
+
+
+//************************** Task : 06 ***************************
+
+// document.write('Counting: ')
+// for(var a = 1; a <= 15; a++){
+// document.write(a + ', ')
+// }
+
+
+// document.write('Even: ')
+// for(var a = 0; a <= 20; a++){
+// if(a%2 == 0){
+//     document.write(a+ ', ' )
+// }
+// }
+
+// document.write('Odd: ')
+// for(var a = 0; a <= 20; a++){
+// if(a%2 !== 0){
+//     document.write(a+ ', ' )
+// }
+// }
+
+
+//************************** Task : 07 ***************************
+
+// var a = prompt('Welcome to our bakery, Search items here');
+// var arr = ['cake', 'biskits', 'rusk', 'cookies', 'patties'];
+// var flag = false;
+
+// for(var i = 0; i < arr.length; i ++){
+//     if(a == arr[i]){
+//         flag = true;
+//         alert(a + ' is available in our bakery');
+//         break;
+//     }
+// }
+
+// if(flag === false){
+//     alert('We are sorry. '+ a + ' is not availabel in our bakery')
+
+// }
+
+
+//************************** Task : 10 ***************************
+
+
+
+// for(var a = 5; a <= 100; a++){
+// if(a%5 == 0){
+//     document.write(a+ ', ' )
+// }
+// }
+
